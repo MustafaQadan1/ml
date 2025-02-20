1af8906c8542280994021b95c21c4553e8d21600
MustafaQadan1 <95106779+MustafaQadan1@users.noreply.github.com>
2025-02-20 10:27:56 +0300

    add

---
+++ main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+import joblib
+import uvicorn
+
+app = FastAPI()
+
+@app.get("/")
+async def read_root(age :int, gender :int):
+   
+    model = joblib.load('music-recommender.joblib')
+    predictions = model.predict([[age, gender]])
+    print(predictions[0])
+    return {"predictions: "+ predictions[0]}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000)
