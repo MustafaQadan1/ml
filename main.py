3dd0ebe6370c4bf9d1fc2031ab2ed4332a38144b
MustafaQadan1 <95106779+MustafaQadan1@users.noreply.github.com>
2025-02-24 13:45:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -13,4 +13,4 @@ async def read_root(age :int, gender :int):
     return {"predictions: "+ predictions[0]}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8090)
+    uvicorn.run(app, host="127.0.0.1", port=8001)
