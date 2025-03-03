24a78890ad4625c2e15e45ae87aeebe0982cfa72
MustafaQadan1 <95106779+MustafaQadan1@users.noreply.github.com>
2025-03-03 10:12:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ app = FastAPI()
 
 # Fetch API key from environment variables
 API_KEY = '34563767897t808909'
+aws_access_key_id = AKIAQYLPMN5HHHFPZAM2
+aws_secret_access_key = 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 
 
 @app.get("/")
 async def read_root(age: int, gender: int, key: str):
